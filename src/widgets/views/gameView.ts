1b681c941071ca31085f39d2bf7c9e9d20202eb5
Charlie Le <20309750+CharlieQLe@users.noreply.github.com>
2023-02-10 20:58:03 -0500

    Add profile switcher

--- src/widgets/views/gameView.ts
+++ src/widgets/views/gameView.ts
@@ -2,17 +2,21 @@ import Adw from 'gi://Adw';
 import GObject from 'gi://GObject';
 import Gtk from 'gi://Gtk';
 
-import { Game } from 'resource:///io/github/charlieqle/Moddy/js/config.js';
+import { Game, Profile } from 'resource:///io/github/charlieqle/Moddy/js/config.js';
 
 export class GameView extends Gtk.Box {
+    private _profileSelector!: Adw.ComboRow;
+
     private _title!: string;
 
     private _game: Game;
+    private _profiles: Profile[];
 
     static {
         GObject.registerClass({
             GTypeName: 'GameView',
             Template: 'resource:///io/github/charlieqle/Moddy/ui/views/game-view.ui',
+            InternalChildren: ['profileSelector'],
             Properties: {
                 'title': GObject.ParamSpec.string('title', 'Title', 'Window title', GObject.ParamFlags.READWRITE, ''),
             },
@@ -22,7 +26,13 @@ export class GameView extends Gtk.Box {
     constructor(game: Game) {
         super();
         this._game = game;
+        this._profiles = [];
         this.title = game.name;
+        const list = this._profileSelector.model as Gtk.StringList;
+        for (const [name, profile] of Object.entries(game.profiles)) {
+            list.append(name);
+            this._profiles.push(profile);
+        }
     }
 
     public get title() {
@@ -32,5 +42,9 @@ export class GameView extends Gtk.Box {
     public set title(title: string) {
         this._title = title;
         this.notify('title');
+    }
+
+    private onProfileSelected(_: Adw.ComboRow, __: any) {
+        //
     }
 }
