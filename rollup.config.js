71c19b3cc578424388b15d2c3d5948e4458bdb2b
Charlie Le <20309750+CharlieQLe@users.noreply.github.com>
2023-02-17 19:59:38 -0500

    Update submodule and bundle d.ts

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,6 @@
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs'
+import dts from 'rollup-plugin-dts';
 
 /* Export like so:
 
@@ -24,6 +25,13 @@ export default [
             file: 'src/vendor/vdf-parser.js',
         },
         plugins: [nodeResolve(), commonjs()],
+    },
+    {
+        input: 'node_modules/@node-steam/vdf/lib/index.d.ts',
+        output: {
+            file: 'src/vendor/vdf-parser.d.ts',
+        },
+        plugins: [dts()],
     },
     {
         input: 'node_modules/steam-acf2json/main.js',
---
+++ src/vendor/vdf-parser.d.ts
@@ -0,0 +1,4 @@
+declare const parse: (text: string) => any;
+declare const stringify: (object: any) => string;
+
+export { parse, stringify };
